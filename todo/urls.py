dd3ab00c585065811fb4349f111e455b64006704
Umesh Mandal <uk.mandal667@hotmail.com>
2024-11-07 00:14:01 +0900

    to do crud final

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,19 @@ from . import views
 
 
 urlpatterns = [
+    # Mark as add task
     path('addtask/', views.addTask, name='addTask'),
+
+    # Mark as done task
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    
+    # Mark as undone task
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+
+    # Mark as edit task
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+
+    # Mark as delete task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
+
 ]
